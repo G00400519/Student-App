9e6284fd14890b16f9b57ac45ebc62f3ae556df4
g00400519 <g00400519@atu.ie>
2022-10-18 13:45:38 +0100

    Prompt user for width

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -8,7 +8,11 @@ public class StudentApp {
         System.out.println("Please enter the width of the rectangle");
         Scanner input = new Scanner(System.in);
         double length = input.nextDouble();
-                System.out.println("You entered: " + length);
+        System.out.println("You entered: " + length);
+
+        System.out.println("Please enter the width of the rectangle");
+        double width = input.nextDouble();
+        System.out.println("You entered: " + width);
     }
 }
 
